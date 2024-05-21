7a62196fae0c2c6335a691182980fcb7d5e22e13
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-21 10:23:46 +0530

    Js oops concepts 1

---
+++ 07.oops_js/07.1_class_&_object/script.js
@@ -0,0 +1,5 @@
+
+
+// *********************** OBJECT ***********************
+
+// Objects: Objects are the fundamental building blocks of JavaScript. They encapsulate data and behavior into a single entity.
