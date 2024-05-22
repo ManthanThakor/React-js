657100f2a43d7f7780b81f65079e8f502687403b
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-22 10:23:17 +0530

    React ES6 Variable

--- 08.ReactES6/08.2_ReactArrowFunctions/script.js
+++ 08.ReactES6/08.2_ReactArrowFunctions/script.js
@@ -18,6 +18,8 @@ hello = () => {
   return "Hello World!";
 }
 
+//=============================================================================================
+
 // It gets shorter! If the function has only one statement, and the statement returns a value, you can remove the brackets and the return keyword:
 
 // Arrow Functions Return Value by Default:
@@ -25,16 +27,25 @@ hello = () => {
 hello = () => "Hello World!";
 
 // ** Note: This works only if the function has only one statement.
+//=============================================================================================
 
 // If you have parameters, you pass them inside the parentheses:
 
 // Arrow Function With Parameters:
 
 hello = (val) => "Hello " + val;
+//=============================================================================================
 
 // In fact, if you have only one parameter, you can skip the parentheses as well:
-
 
 // Arrow Function Without Parentheses:
 
 hello = val => "Hello " + val;
+
+//=============================================================================================
+
+// If you have more than one parameter, you have to use the parentheses:
+
+// Arrow Function With Multiple Parameters:
+
+hello = (val1, val2) => "Hello " + val1 + " and " + val2;
