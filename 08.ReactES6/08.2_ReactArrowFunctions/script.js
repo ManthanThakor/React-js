b45a1524f58ff6dcb1605606fb61987d707f20b1
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-22 09:51:07 +0530

    React ES6 ARROW FUNCTION

---
+++ 08.ReactES6/08.2_ReactArrowFunctions/script.js
@@ -0,0 +1,40 @@
+
+
+// Arrow Functions
+// Arrow functions allow us to write shorter function syntax:
+
+// ExampleGet your own React.js Server
+// Before:
+
+hello = function() {
+  return "Hello World!";
+}
+
+//=============================================================================================
+
+// *********** With Arrow Function: ***********
+
+hello = () => {
+  return "Hello World!";
+}
+
+// It gets shorter! If the function has only one statement, and the statement returns a value, you can remove the brackets and the return keyword:
+
+// Arrow Functions Return Value by Default:
+
+hello = () => "Hello World!";
+
+// ** Note: This works only if the function has only one statement.
+
+// If you have parameters, you pass them inside the parentheses:
+
+// Arrow Function With Parameters:
+
+hello = (val) => "Hello " + val;
+
+// In fact, if you have only one parameter, you can skip the parentheses as well:
+
+
+// Arrow Function Without Parentheses:
+
+hello = val => "Hello " + val;
