0efd5a4c497e9d0016fc658e726a45add9cad45b
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-26 22:12:03 +0530

    react class

---
+++ 12.react-class/12.2_practiceofclass/vitrect/src/componet/welcom.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class welcome extends Component {
+    render() {
+        return (
+          <div>
+            <h1>Hello, World!</h1>
+            <p>This is a class component.</p>
+          </div>
+        );
+      }
+    }
+    
+    export default welcome;
