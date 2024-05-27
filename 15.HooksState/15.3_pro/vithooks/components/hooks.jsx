d88d60ee512cccdae8672c2c9b08a31cb6f204ab
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-27 19:02:01 +0530

    react hooks and state 4

---
+++ 15.HooksState/15.3_pro/vithooks/components/hooks.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("orange");
+
+  return (
+    <>
+      <h1 style={{ color: color }}>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+      <button
+        type="button"
+        onClick={() => setColor("red")}
+      >Red</button>
+      <button
+        type="button"
+        onClick={() => setColor("pink")}
+      >Pink</button>
+      <button
+        type="button"
+        onClick={() => setColor("green")}
+      >Green</button>
+    </>
+  );
+}
+
+export default FavoriteColor;
--- 15.HooksState/15.3_pro/vithooks/src/App.jsx
+++ 15.HooksState/15.3_pro/vithooks/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FavoriteColor from '../components/hooks'
 
 function App() {
   const [count, setCount] = useState(10)
@@ -14,6 +15,7 @@ function App() {
       <button onClick={()=>{setCount(count + 1)}}>Update count</button>
       <button onClick={()=>{setCountt(counttwo ** 2)}}>Update count two</button>
 
+<FavoriteColor/>
     </>
   )
 }
