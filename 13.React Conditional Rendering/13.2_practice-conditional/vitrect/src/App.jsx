ad461a331e0a44537a053ee51c8ab6b684c45198
Manthan Thakor <thakormanthan849@gmail.com>
2024-05-27 11:15:55 +0530

    react conditional Rendering 7

--- 13.React Conditional Rendering/13.2_practice-conditional/vitrect/src/App.jsx
+++ 13.React Conditional Rendering/13.2_practice-conditional/vitrect/src/App.jsx
@@ -1,13 +1,48 @@
-
-import './App.css'
+import React, { useState } from 'react';
+import IfStatementComponent from '../components/IfStatementComponent';
+import TernaryOperatorComponent from '../components/TernaryOperatorComponent';
+import LogicalAndComponent from '../components/LogicalAndComponent';
+import IIFEComponent from '../components/IIFEComponent';
+import SwitchCaseComponent from '../components/SwitchCaseComponent';
+import RenderPropsComponent from '../components/RenderPropsComponent';
+import InlineConditionalComponent from '../components/InlineConditionalComponent';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [status, setStatus] = useState('loading');
 
   return (
-    <>
+    <div>
+      <h2>If Statement Component</h2>
+      <IfStatementComponent isLoggedIn={isLoggedIn} />
 
-    </>
-  )
+      <h2>Ternary Operator Component</h2>
+      <TernaryOperatorComponent isLoggedIn={isLoggedIn} />
+
+      <h2>Logical AND Component</h2>
+      <LogicalAndComponent isLoggedIn={isLoggedIn} />
+
+      <h2>IIFE Component</h2>
+      <IIFEComponent isLoggedIn={isLoggedIn} />
+
+      <h2>Switch Case Component</h2>
+      <SwitchCaseComponent status={status} />
+
+      <h2>Render Props Component</h2>
+      <RenderPropsComponent isLoggedIn={isLoggedIn} />
+
+      <h2>Inline Conditional Component</h2>
+      <InlineConditionalComponent isLoggedIn={isLoggedIn} />
+
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? 'Logout' : 'Login'}
+      </button>
+      <br></br><br></br>
+      <button onClick={() => setStatus('loading')}>Set Loading</button><br></br><br></br>
+      <button onClick={() => setStatus('success')}>Set Success</button><br></br><br></br>
+      <button onClick={() => setStatus('error')}>Set Error</button><br></br><br></br><br></br><br></br><br></br><br></br>
+    </div>
+  );
 }
 
-export default App
+export default App;
