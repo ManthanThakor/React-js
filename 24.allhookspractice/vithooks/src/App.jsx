0a559745cc8f107c09c4fd1bbff71dd34842f9b4
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-12 11:46:05 +0530

    React All hooks pra 3

--- 24.allhookspractice/vithooks/src/App.jsx
+++ 24.allhookspractice/vithooks/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import UseState from './components/usestate/UseState'
-import UseStateObject from './components/usestate/UseStateObject'
+import "./App.css";
+import UseState from "./components/usestate/UseState";
+import UseStateObject from "./components/usestate/UseStateObject";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <UseState/>
-      <UseStateObject/>
+      <hr></hr>
+
+      <h1>UseState</h1>
+      <hr></hr>
+      <UseState />
+      <hr></hr>
+
+      <h1>UseStateObject</h1>
+      <hr></hr>
+
+      <UseStateObject />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 24.allhookspractice/vithooks/src/components/usestate/UseStateObject.jsx
+++ 24.allhookspractice/vithooks/src/components/usestate/UseStateObject.jsx
@@ -11,12 +11,12 @@ const UseStateObject = () => {
 
   const handleInput = (event) => {
     const name = event.target.name;
-
     const value = event.target.value;
 
     setFormData((prev) => {
       return { ...prev, [name]: value };
     });
+    console.log(formData);
   };
 
   return (
