af467ca4968c770596a608f556150cc2cd0037ac
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-13 11:42:16 +0530

    React All hooks pra 14

--- 24.allhookspractice/vithooks/src/App.jsx
+++ 24.allhookspractice/vithooks/src/App.jsx
@@ -9,6 +9,8 @@ import Parent from "./Parent";
 import ReducerHook from "./components/usereducer/ReducerHook";
 import RefHook1 from "./components/useref/RefHook1";
 import RefHook from "./components/useref/RefHook";
+import LayoutEffect from "./components/uselayouteffect/LayoutEffect";
+import MemoHook from "./components/usememo/MemoHook";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -44,6 +46,14 @@ function App() {
       <hr></hr>
       <RefHook1 />
       <RefHook />
+      <hr></hr>
+      <h1>LayoutEffect</h1>
+      <hr></hr>
+      <LayoutEffect />
+      <hr></hr>
+      <h1>MemoHook</h1>
+      <hr></hr>
+      <MemoHook />
     </>
   );
 }
