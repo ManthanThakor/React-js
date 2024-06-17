bf44cdfe06286e88bded107183934446f1512590
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-17 12:00:21 +0530

    React API 6

--- 25.reactapi/25.2_pra/Axios/src/components/axiostutorial.jsx
+++ 25.reactapi/25.2_pra/Axios/src/components/axiostutorial.jsx
@@ -1,23 +1,20 @@
-
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function Axiostutorial() {
-    
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/users")
-        .then((response) => {
-          console.log(response);
-        })
-        
-      }, [])
+  const [userdata, setdata] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      console.log(response);
+      setdata(response.data);
+    });
+  }, []);
   return (
-
     <div>
-      
-
-  
+      {userdata.map((data) => {
+        return <div> {data.name} </div>;
+      })}
     </div>
-  )
+  );
 }
 
 export default Axiostutorial;
