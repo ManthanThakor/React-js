34e4f59e7badf76746a2d7055143ff2220ac05f2
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-17 23:07:59 +0530

    React API 10

--- 25.reactapi/25.2_pra/Axios/src/components/axiospost.jsx
+++ 25.reactapi/25.2_pra/Axios/src/components/axiospost.jsx
@@ -4,8 +4,9 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 const Axiospost = () => {
-  const data  = { fname: "", lastName: "" };
-  const [inputData, setInputData] = useState(data);
+  const initialData = { fname: "", lastName: "" };
+  const [inputData, setInputData] = useState(initialData);
+  const [apiResponse, setApiResponse] = useState(null);
 
   const handleData = (event) => {
     const name = event.target.name;
@@ -19,9 +20,39 @@ const Axiospost = () => {
     axios.post("https://jsonplaceholder.typicode.com/users", inputData)
       .then((response) => {
         console.log(response);
+        setApiResponse(response.data);
       })
       .catch((error) => {
         console.error("There was an error!", error);
+        setApiResponse({ error: "There was an error!" });
+      });
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    console.log(inputData);
+    axios.put("https://jsonplaceholder.typicode.com/users/1", inputData)
+      .then((response) => {
+        console.log(response);
+        setApiResponse(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        setApiResponse({ error: "There was an error!" });
+      });
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    console.log(inputData);
+    axios.delete("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => {
+        console.log(response);
+        setApiResponse({ message: "User deleted successfully" });
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        setApiResponse({ error: "There was an error!" });
       });
   };
 
@@ -32,6 +63,14 @@ const Axiospost = () => {
       <label>Last Name:</label>
       <input type="text" name="lastName" value={inputData.lastName} onChange={handleData} />
       <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleUpdate}>Update</button>
+      <button onClick={handleDelete}>Delete</button>
+
+      {apiResponse && (
+        <div className="api-response">
+          <pre>{JSON.stringify(apiResponse, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 };
