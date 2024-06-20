5d5c2a5d5580ce066ef970fad9f8a1322c1f0a21
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-20 10:58:46 +0530

    React Routerr 10

--- 26.reactroutermore/Router/src/main.jsx
+++ 26.reactroutermore/Router/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.jsx';
 import './index.css';
 import Home from './components/Home/Home.jsx';
@@ -10,16 +11,20 @@ import Contact from './components/Contact/Contact.jsx';
 import User from './components/User/User.jsx';
 import Github from './components/Github/Github.jsx';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="user" element={<User />} />
-        <Route path="github" element={<Github />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="user" element={<User />} />
+          <Route path="github" element={<Github />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
